b8cf6b3defd6d76e40feef5d4a3c0c5a9f47d91b
matheusfabio08 <mmatheuus@hotmail.com>
2026-07-01 21:24:36 -0300

    feat: adiciona rota /admin/ficha-editor (AdminRoute)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,6 +24,7 @@ import FabricQuality from '@/pages/FabricQuality'
 import ListaSaida    from '@/pages/ListaSaida'
 import Settings      from '@/pages/Settings'
 import Admin         from '@/pages/Admin'
+import FichaEditor  from '@/components/FichaEditor/FichaEditor'
 
 const qc = new QueryClient({
   defaultOptions: { queries: { staleTime: 30_000, retry: 1 } },
@@ -51,26 +52,27 @@ export default function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-            <Route path="/dashboard"      element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-            <Route path="/gerenciamento"  element={<PrivateRoute><Gerenciamento /></PrivateRoute>} />
-            <Route path="/pcp"            element={<PrivateRoute><PCP /></PrivateRoute>} />
-            <Route path="/almoxarifado"   element={<PrivateRoute><Almoxarifado /></PrivateRoute>} />
-            <Route path="/laboratorio"    element={<PrivateRoute><Laboratorio /></PrivateRoute>} />
-            <Route path="/pesagem"        element={<PrivateRoute><Pesagem /></PrivateRoute>} />
-            <Route path="/preparacao"     element={<PrivateRoute><Preparacao /></PrivateRoute>} />
-            <Route path="/preparacao-lote" element={<PrivateRoute><PreparacaoLote /></PrivateRoute>} />
-            <Route path="/producao"       element={<PrivateRoute><Producao /></PrivateRoute>} />
-            <Route path="/box4"           element={<PrivateRoute><Box4 /></PrivateRoute>} />
-            <Route path="/box5"           element={<PrivateRoute><Box5 /></PrivateRoute>} />
-            <Route path="/box6"           element={<PrivateRoute><Box6 /></PrivateRoute>} />
-            <Route path="/secadora"       element={<PrivateRoute><Secadora /></PrivateRoute>} />
-            <Route path="/destrinchagem"  element={<PrivateRoute><Destrinchagem /></PrivateRoute>} />
-            <Route path="/enrolagem"      element={<PrivateRoute><Enrolagem /></PrivateRoute>} />
-            <Route path="/qualidade"      element={<PrivateRoute><Qualidade /></PrivateRoute>} />
-            <Route path="/fabric-quality" element={<PrivateRoute><FabricQuality /></PrivateRoute>} />
-            <Route path="/lista-saida"    element={<PrivateRoute><ListaSaida /></PrivateRoute>} />
-            <Route path="/settings"       element={<PrivateRoute><Settings /></PrivateRoute>} />
-            <Route path="/admin"          element={<AdminRoute><Admin /></AdminRoute>} />
+            <Route path="/dashboard"           element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/gerenciamento"        element={<PrivateRoute><Gerenciamento /></PrivateRoute>} />
+            <Route path="/pcp"                  element={<PrivateRoute><PCP /></PrivateRoute>} />
+            <Route path="/almoxarifado"         element={<PrivateRoute><Almoxarifado /></PrivateRoute>} />
+            <Route path="/laboratorio"          element={<PrivateRoute><Laboratorio /></PrivateRoute>} />
+            <Route path="/pesagem"              element={<PrivateRoute><Pesagem /></PrivateRoute>} />
+            <Route path="/preparacao"           element={<PrivateRoute><Preparacao /></PrivateRoute>} />
+            <Route path="/preparacao-lote"      element={<PrivateRoute><PreparacaoLote /></PrivateRoute>} />
+            <Route path="/producao"             element={<PrivateRoute><Producao /></PrivateRoute>} />
+            <Route path="/box4"                 element={<PrivateRoute><Box4 /></PrivateRoute>} />
+            <Route path="/box5"                 element={<PrivateRoute><Box5 /></PrivateRoute>} />
+            <Route path="/box6"                 element={<PrivateRoute><Box6 /></PrivateRoute>} />
+            <Route path="/secadora"             element={<PrivateRoute><Secadora /></PrivateRoute>} />
+            <Route path="/destrinchagem"        element={<PrivateRoute><Destrinchagem /></PrivateRoute>} />
+            <Route path="/enrolagem"            element={<PrivateRoute><Enrolagem /></PrivateRoute>} />
+            <Route path="/qualidade"            element={<PrivateRoute><Qualidade /></PrivateRoute>} />
+            <Route path="/fabric-quality"       element={<PrivateRoute><FabricQuality /></PrivateRoute>} />
+            <Route path="/lista-saida"          element={<PrivateRoute><ListaSaida /></PrivateRoute>} />
+            <Route path="/settings"             element={<PrivateRoute><Settings /></PrivateRoute>} />
+            <Route path="/admin"                element={<AdminRoute><Admin /></AdminRoute>} />
+            <Route path="/admin/ficha-editor"   element={<AdminRoute><FichaEditor /></AdminRoute>} />
 
             <Route path="*" element={<Navigate to="/dashboard" replace />} />
           </Routes>
